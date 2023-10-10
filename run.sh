013d884ce57b082178c8296ee420ff1fe5b4aa7f
Fw3325 <146174404+Fw3325@users.noreply.github.com>
2023-10-10 07:43:29 -0500

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
-BASE_DIR = '/root/autodl-tmp/BertClassNews'
-ECHO $BASE_DIR
+BASE_DIR=/root/autodl-tmp/BertClassNews
+echo ${BASE_DIR}
 
-cd {BASE_DIR}/load_dataset
+# cd ${BASE_DIR}/load_dataset
 
-ECHO "开始处理数据" 
-nohup python3 load_data_cls.py
+echo "开始处理数据" 
+nohup python3 load_dataset/load_data_cls.py >> log.log 2>&1 &
 
-cd ..
-cd {BASE_DIR}/train
-ECHO "开始训练模型"
-nohup python3 train_eval.py
+# cd ..
+# cd ${BASE_DIR}/train
+echo "开始训练模型"
+nohup python3 train/train_eval.py >> log.log 2>&1 &
 
-ECHO "预测模型"
-nohup python3 predict.py
+echo "预测模型"
+nohup python3 train/predict.py >> log.log 2>&1 &
